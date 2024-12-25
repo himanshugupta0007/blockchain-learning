547cc6087f74798dd1bcb36da7c3364425d3e05f
HimanshuGupta <himanshu.gupta@gigaforce.io>
2024-12-25 16:43:40 +0530

    Adding API for blockchain

--- blockchain-practice/app/app.js
+++ blockchain-practice/app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgon = require("morgan");
+const blockChainRoute = require("../app/route/blockchainrouter");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(morgon("dev"));
 // encoded url
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/blockchain", blockChainRoute);
 
 module.exports = app;
--- blockchain-practice/app/controller/blockchaincontroller.js
+++ blockchain-practice/app/controller/blockchaincontroller.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const httpStatus = require("http-status-codes");
-const BlockChain = require("../blockchain/blockchain");
+const BlockChain = require("../../blockchain/blockchain");
 
-const blockchain = new Blockchain();
+const blockchain = new BlockChain();
 
 exports.getBlocks = (req, res) => {
     res.status(httpStatus.StatusCodes.OK).send(blockchain);
